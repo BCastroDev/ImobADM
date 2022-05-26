727221befb920f338e26c3ccf013c82b788800c0
Bruno Gráfica <servicosbruno@gmail.com>
2022-05-26 13:11:29 -0300

    PassandoID

--- public/scripts/click.js
+++ public/scripts/click.js
@@ -3,16 +3,10 @@ const app = express();
 const imovel = require('./imovel') //esse imovel é a modelagem que criamos do db
 const bodyParser = require('body-parser');
 
- clickCard = (id)=> {
+ function clickCard(id) {
     console.log(id)
-    app.delete('/delete/:id', async (req,res)=>{
-        await Imovel.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-    })
+ 
+    }
     // let modal = event.target;
     // modal.style.display = "none"
     
-}
