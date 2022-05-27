e09cabf1696e53d6ea5ac9f86f500317d607190f
Bruno Gráfica <servicosbruno@gmail.com>
2022-05-27 15:45:55 -0300

    teste

--- public/scripts/click.js
+++ public/scripts/click.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 function clickCard(event) {
     console.log(event.target)
     let modal = event.target;
     modal.style.color = "green"
     
 }
-=======
-const express = require('express');
-const app = express();
-const imovel = require('./imovel') //esse imovel é a modelagem que criamos do db
-const bodyParser = require('body-parser');
-
- function clickCard(id) {
-    console.log(id)
- 
-    }
-    // let modal = event.target;
-    // modal.style.display = "none"
-    
->>>>>>> b4cee76110c5eda4431116a7a811169d8d9f3166
