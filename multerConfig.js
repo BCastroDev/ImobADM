11f604ee4f660b3cd6a8536f7bd4426a1fda7ed2
Bruno Gráfica <servicosbruno@gmail.com>
2022-06-17 14:20:55 -0300

    Os arquivos vão pra pasta mas estou com dificuldade de fazer irem para pastas diferentes dependendo da vez

--- multerConfig.js
+++ multerConfig.js
@@ -2,30 +2,33 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const { dir } = require('console');
-var imovelId = 4
 
-// var finalName = "esse"
+
+var imgFolder = 1;
 
 const storage = multer.diskStorage(
     {
         destination: (req, file, callback)=>{
-
-            if (!fs.existsSync(imovelId)){
-                //Efetua a criação do diretório
-                fs.mkdirSync(path.resolve("uploads")+`/${imovelId}`)
+            
+            if (!fs.existsSync(path.resolve("uploads")+`/${imgFolder}`)){     //Efetua a criação do diretório
+                fs.mkdirSync(path.resolve("uploads")+`/${imgFolder}`)
+                console.log("A pasta", imgFolder ,"não existia e foi criado")
             }
-            
+            else  {
+                console.log("A pasta", imgFolder , "já existia")
 
-
-            callback(null, path.resolve("uploads")+`/${imovelId}`)
-            imovelId++}, //Aqui vai a pasta
-            
+            }
+            callback(null, path.resolve("uploads")+`/${imgFolder}`)
+            }, //Aqui vai a pasta
+        
             filename:(req, file, callback)=>{
                 const time=new Date().getTime();
                 let finalName = `${time}_${file.originalname}`
-                callback(null,finalName);
-            }
-            });
+                callback(null,finalName)
+
+            }});
+
+
 
 
             
